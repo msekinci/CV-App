81b0d525cb5dd6b4cb6ce168dc2f7d32998597ad
Mehmet Serkan Ekinci <mehmetserkanekinci@gmail.com>
2021-03-16 23:44:23 +0300

    data acces layer and repository pattern added

---
+++ MSE.CVApp.DataAccess/Concrete/Dapper/DPGenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using Dapper.Contrib.Extensions;
+using MSE.CVApp.DataAccess.Interfaces;
+using MSE.CVApp.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace MSE.CVApp.DataAccess.Concrete.Dapper
+{
+    public class DPGenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public DPGenericRepository(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbConnection.Delete(entity);
+        }
+
+        public List<TEntity> GetAll()
+        {
+            return _dbConnection.GetAll<TEntity>().ToList();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _dbConnection.Get<TEntity>(id);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            _dbConnection.Insert(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbConnection.Update(entity);
+        }
+    }
+}
---
+++ MSE.CVApp.DataAccess/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using MSE.CVApp.Entities.Interfaces;
+using System.Collections.Generic;
+
+namespace MSE.CVApp.DataAccess.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        List<TEntity> GetAll();
+        TEntity GetById(int id);
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
