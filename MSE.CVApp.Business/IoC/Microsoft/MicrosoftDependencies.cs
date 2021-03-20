01ae8be6c71e018e364fdb795219fc3952c88253
Mehmet Serkan Ekinci <mehmetserkanekinci@gmail.com>
2021-03-20 10:20:40 +0300

    microsoft dependencies

---
+++ MSE.CVApp.Business/IoC/Microsoft/MicrosoftDependencies.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MSE.CVApp.Business.IoC.Microsoft
+{
+    public static class MicrosoftDependencies
+    {
+        public static void AddCustomDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IDbConnection>(con => new SqlConnection(configuration.GetConnectionString("connectionMssql")));
+        }
+    }
+}
